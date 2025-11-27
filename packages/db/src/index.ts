0793e55c8c8d4303f6930060cea5e7fdd26940a7
Ayushman Singh <ayushmans012@gmail.com>
2025-11-27 23:33:31 +0530

    db package completed

---
+++ packages/db/src/index.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '../generated/prisma/client';
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+    accelerateUrl: process.env.ACCELERATE_URL ?? '',
+  })
+// to prevent hot reloading from creating new instances of PrismaClient.....read more in the last comment
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// to fix the error that we were gettting we added "types": ["node"]  in the typescript-config/base.json file
+
+export * from '../generated/prisma/client'
+
+
+// read about above from the following link : https://www.prisma.io/docs/orm/prisma-client/setup-and-configuration/databases-connections#prevent-hot-reloading-from-creating-new-instances-of-prismaclient
