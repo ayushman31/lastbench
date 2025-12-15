18fef3916030ae6b32694f7ef243e42acb865498
Ayushman Singh <ayushmans012@gmail.com>
2025-12-16 02:46:59 +0530

    exports and env variables added

---
+++ packages/storage/src/index.ts
@@ -0,0 +1,4 @@
+export { StorageClient, getStorageClient } from './client';
+export { getStorageConfig } from './config';
+export type { StorageConfig } from './config';
+export { getMimeType, formatBytes, validateFileSize } from './utils';
